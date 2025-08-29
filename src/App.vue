2e34c7e899dafdc6a70d44de7276926d002db2b1
Mate Molnar <molnar.mate@zengo.eu>
2025-08-29 14:52:36 +0200

    updated styles

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,12 @@
   <main class="p-8 space-y-10 max-w-screen-lg mx-auto">
     <section>
       <h2 class="text-lg font-medium mb-2">Reusable Table Test</h2>
-      <ReusableTable :config="explicit" />
+      <ReusableTable :config="reusableTableConfiguration" />
     </section>
   </main>
 </template>
 
 <script setup lang="ts">
 import ReusableTable from "./components/ReusableTable.vue";
-import { explicit } from "./configs/ReusableTableConfig";
+import { reusableTableConfiguration } from "./configs/ReusableTableConfig";
 </script>
